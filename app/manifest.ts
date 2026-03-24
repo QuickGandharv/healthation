97ead6e2dbbe14302409d054374b34d0a2a69b79
QuickGandharv <gandharvakalia@gmail.com>
2026-03-24 17:47:52 +0530

    feat: add PWA manifest and Android app icon for PWA support

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/assets/icon/app-icon.png',
+        src: '/assets/icon/android-app-icon.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/assets/icon/app-icon.png',
+        src: '/assets/icon/android-app-icon.png',
         sizes: '512x512',
         type: 'image/png',
       },
